1a16f954146a7bd8fbd73c271b89c699d59ad195
kesac <kevin@turtlesort.com>
2017-09-04 12:04:03 -0600

    Added helper extension methods for managing Xml nodes

---
+++ Glam.Desktop/Utility/XmlHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Glam.Desktop.Utility
+{
+    public static class XmlHelpers
+    {
+        public static bool Is(this XElement e, string name)
+        {
+            return e.Name == name;
+        }
+
+        public static string Get(this XElement e, string attribute)
+        {
+            return e.Attribute(attribute).Value;
+        }
+    }
+}
